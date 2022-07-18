4031282937a6a41b09bc29569cab17505787b325
Abby Winn <abbywinn14@gmail.com>
2022-07-18 15:00:20 -0500

    Removed inline styling in "title" and "searchBar"

--- src/components/admin/searchBar.js
+++ src/components/admin/searchBar.js
@@ -6,22 +6,31 @@ import IconButton from '@mui/material/IconButton';
 import SearchIcon from '@mui/icons-material/Search';
 
 export default function SearchBar() {
+  const searchBarPosition = {
+    marginBottom: '1%',
+    display: 'flex',
+    alignItems: 'right',
+    width: '15%',
+    marginLeft: '60%',
+    bgcolor: '#F5F5F5',
+    marginTop: '1%',
+  };
+  const inputBaseStyle = {
+    ml: 1,
+    flex: 1,
+  };
+  const dividerStyle = {
+    height: 28,
+    m: 0.5,
+  };
+  const buttonStyle = {
+    p: '10px',
+  };
   return (
-    <Paper
-      component="form"
-      sx={{
-        marginBottom: '1%',
-        display: 'flex',
-        alignItems: 'right',
-        width: '15%',
-        marginLeft: '60%',
-        bgcolor: '#F5F5F5',
-        marginTop: '1%',
-      }}
-    >
-      <InputBase sx={{ ml: 1, flex: 1 }} placeholder="Filter..." />
-      <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
-      <IconButton color="primary" sx={{ p: '10px' }}>
+    <Paper component="form" sx={searchBarPosition}>
+      <InputBase sx={inputBaseStyle} placeholder="Filter..." />
+      <Divider sx={dividerStyle} orientation="vertical" />
+      <IconButton sx={buttonStyle} color="primary">
         <SearchIcon />
       </IconButton>
     </Paper>
