ba4fdcf917af2a3dd2efa4ee36af284303b941f0
Andrew Prudlow <andrewprudlow@gmail.com>
2022-07-21 10:00:19 -0500

    Update studentregistrymodal

--- src/components/students/studentRegistryModal.js
+++ src/components/students/studentRegistryModal.js
@@ -11,6 +11,8 @@ import Modal from '@mui/material/Modal';
 import Stack from '@mui/material/Stack';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
+import { useNavigate } from 'react-router';
+import ROUTES from '../../constants/routes';
 
 export default function StudentRegistryModal(props) {
   const {
@@ -46,11 +48,16 @@ export default function StudentRegistryModal(props) {
     confirmHandler(firstName, lastName, email, phoneNumber, dateOfBirth);
     handleClose();
   };
+  const navigate = useNavigate();
 
   return (
     <div>
       <p />
-      <Button onClick={handleOpen} variant="contained" startIcon={<AddIcon />}>
+      <Button
+        onClick={() => navigate(ROUTES.SIGN_UP)}
+        variant="contained"
+        startIcon={<AddIcon />}
+      >
         Register Student
       </Button>
       <Modal open={open} onClose={handleClose}>
