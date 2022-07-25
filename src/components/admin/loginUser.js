8c8f681944dc373a7f984bcc253eb0fa467db105
Tommy Terry <tommyterry2005@gmail.com>
2022-07-25 14:54:54 -0500

    Small fix

--- src/components/admin/loginUser.js
+++ src/components/admin/loginUser.js
@@ -1,20 +1,9 @@
 import GetID from '../../util/id/get-id';
-import {
-    // getAdmins,
-    // addAdmin,
-    // updateAdmin,
-     getAdminById,
-  } from '../../services/Admin/admin';
 
-export default function loginUser(props){
-    const user = getAdminById(GetID());
-    console.log(GetID());
-    console.log(user);
-    console.log(user.userName);
-
-    return (
-        <div>
-            <p>Hello {GetID()}</p>
-        </div>
-    );
+export default function loginUser(props) {
+  return (
+    <div>
+      <p>Hello {GetID()}</p>
+    </div>
+  );
 }
