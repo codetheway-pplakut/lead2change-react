dcabf241eb6094cd8c12d9508cd36bf17ed2dd2a
Sirat Mokha <mokha1671@gmail.com>
2022-07-25 11:34:42 -0700

    Editable fields in Details tab

--- src/components/students/detailsTab.js
+++ src/components/students/detailsTab.js
@@ -15,6 +15,11 @@ import { styled } from '@mui/material/styles';
 import Button from '@mui/material/Button';
 import { useNavigate } from 'react-router-dom';
 import AppBar from '@mui/material/AppBar';
+import InputLabel from '@mui/material/InputLabel';
+import FormControl from '@mui/material/FormControl';
+import NativeSelect from '@mui/material/NativeSelect';
+import MenuItem from '@mui/material/MenuItem';
+import Select from '@mui/material/Select';
 import ROUTES from '../../constants/routes';
 
 function TabPanel(props) {
@@ -24,8 +29,8 @@ function TabPanel(props) {
     <div
       role="tabpanel"
       hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
+      id={`full-width-tabpanel-${index}`}
+      aria-labelledby={`full-width-tab-${index}`}
       // eslint-disable-next-line react/jsx-props-no-spreading
       {...other}
     >
@@ -47,8 +52,8 @@ TabPanel.propTypes = {
 
 function a11yProps(index) {
   return {
-    id: `simple-tab-${index}`,
-    'aria-controls': `simple-tabpanel-${index}`,
+    id: `full-width-tab-${index}`,
+    'aria-controls': `full-width-tabpanel-${index}`,
   };
 }
 const GridText = styled(Paper)(({ theme }) => ({
@@ -90,13 +95,7 @@ function SignUpDisplay(props) {
             <Tab label="Other" {...a11yProps(2)} />
           </Tabs>
         </AppBar>
-        <Button
-          style={{ float: 'right' }}
-          variant="contained"
-          onClick={onEditClick}
-        >
-          Edit
-        </Button>
+
         <TabPanel value={value} index={0} style={{ overflowY: 'auto' }}>
           <Box sx={{ flexGrow: 1 }}>
             <Grid container justifyContent="flex-end">
@@ -227,6 +226,13 @@ function SignUpDisplay(props) {
           </Box>
         </TabPanel>
       </Box>
+      <Button
+        style={{ float: 'right' }}
+        variant="contained"
+        onClick={onEditClick}
+      >
+        Edit
+      </Button>
     </div>
   );
 }
@@ -275,49 +281,195 @@ function SignUpEdit(props) {
                 sx={{ minWidth: 10, height: 'max-content' }}
                 style={{ height: '64vh' }}
               >
-                <GridText>
+                <Grid>
                   <h3 style={{ color: '#2656A5' }}>Post Secondary Plan</h3>
-                  <h5>
-                    Plans After College: After College I plan to go and get a
-                    job as a Software Developer at a company such as google.
-                  </h5>
-                  <h5>I have applied to a college: Yes</h5>
-                  <h5>
-                    Colleges I’ve applied to/plan to apply to: 1. UW-Madison 2.
-                    Northwestern 3. Purdue
-                  </h5>
-                  <h5>I have begun my work on my college essay: Yes</h5>
-                  <h5>I need help writing my college essay: No</h5>
-                  <h5>First choice of college: Northwestern</h5>
-                  <h3 style={{ color: '#2656A5' }}>
-                    College Entrance Exam Information:
-                  </h3>
-                  <h5>
-                    PACT Score: 35
-                    <div>Date of PACT: 10/22/2021</div>
-                  </h5>
-                  <h5>
-                    PSAT Score: 1500
-                    <div> Date of PSAT: 11/29/2021</div>
-                  </h5>
-                  <h5>
-                    ACT Score: 34
-                    <div> Date of ACT: 3/3/2022</div>
-                  </h5>
-                  <h5>
-                    SAT Score: 1580
-                    <div> Date of SAT: 5/19/2022 </div>
-                  </h5>
-                  <h3 style={{ color: '#2656A5' }}>Financial Aid:</h3>
-                  <h5>
-                    I have already completed the financial aid process: No
-                  </h5>
-                  <h5>
-                    I need assistance filling out my FAFSA/Financial aid forms:
-                    No
-                  </h5>
-                  <h5>Support they need: None</h5>
-                </GridText>
+                  <TextField
+                    size="small"
+                    className="typing-container"
+                    defaultValue="After college i plan to go and get a job as a Software Developer at a company such as google."
+                    label="Plans after college"
+                  />
+                </Grid>
+                <h5>
+                  Colleges I’ve applied to/plan to apply to: 1. UW-Madison 2.
+                  Northwestern 3. Purdue
+                </h5>
+                <Grid>
+                  <TextField
+                    size="small"
+                    className="typing-container"
+                    defaultValue="Northwestern"
+                    label="College First Choice"
+                  />
+                </Grid>
+                <Grid marginTop={1}>
+                  <FormControl variant="standard" sx={{ m: 1, minWidth: 200 }}>
+                    <InputLabel id="demo-simple-select-standard-label">
+                      Have Applied To College
+                    </InputLabel>
+                    <Select
+                      labelId="demo-simple-select-standard-label"
+                      id="demo-simple-select-standard"
+                      label="Age"
+                      defaultValue={10}
+                    >
+                      <MenuItem value=""></MenuItem>
+                      <MenuItem value={10}>Yes</MenuItem>
+                      <MenuItem value={20}>No</MenuItem>
+                    </Select>
+                  </FormControl>
+                </Grid>
+
+                <Grid>
+                  <FormControl variant="standard" sx={{ m: 1, minWidth: 200 }}>
+                    <InputLabel id="demo-simple-select-standard-label">
+                      Started College Essay
+                    </InputLabel>
+                    <Select
+                      labelId="demo-simple-select-standard-label"
+                      id="demo-simple-select-standard"
+                      label="Age"
+                      defaultValue={10}
+                    >
+                      <MenuItem value=""></MenuItem>
+                      <MenuItem value={10}>Yes</MenuItem>
+                      <MenuItem value={20}>No</MenuItem>
+                    </Select>
+                  </FormControl>
+                </Grid>
+                <Grid>
+                  <FormControl variant="standard" sx={{ m: 1, minWidth: 200 }}>
+                    <InputLabel id="demo-simple-select-standard-label">
+                      Need Help With College Essay
+                    </InputLabel>
+                    <Select
+                      labelId="demo-simple-select-standard-label"
+                      id="demo-simple-select-standard"
+                      label="Age"
+                      defaultValue={10}
+                    >
+                      <MenuItem value=""></MenuItem>
+                      <MenuItem value={10}>Yes</MenuItem>
+                      <MenuItem value={20}>No</MenuItem>
+                    </Select>
+                  </FormControl>
+                </Grid>
+
+                <h3 style={{ color: '#2656A5' }}>
+                  College Entrance Exam Information:
+                </h3>
+                <Grid marginBottom={2}>
+                  <TextField
+                    size="small"
+                    className="typing-container"
+                    defaultValue="35"
+                    label="PACT Score"
+                    type="number"
+                  />
+                  <TextField
+                    size="small"
+                    className="typing-container"
+                    label="PSAT Date"
+                    type="date"
+                  />
+                </Grid>
+                <Grid marginBottom={2}>
+                  <TextField
+                    size="small"
+                    className="typing-container"
+                    defaultValue="35"
+                    label="PSAT Score"
+                    type="number"
+                  />
+                  <TextField
+                    size="small"
+                    className="typing-container"
+                    label="PSAT Date"
+                    type="date"
+                  />
+                </Grid>
+                <Grid marginBottom={2}>
+                  <TextField
+                    size="small"
+                    className="typing-container"
+                    defaultValue="35"
+                    label="ACT Score"
+                    type="number"
+                  />
+                  <TextField
+                    size="small"
+                    className="typing-container"
+                    label="ACT Date"
+                    type="date"
+                  />
+                </Grid>
+                <Grid marginBottom={2}>
+                  <TextField
+                    size="small"
+                    className="typing-container"
+                    defaultValue="35"
+                    label="SAT Score"
+                    type="number"
+                  />
+                  <TextField
+                    size="small"
+                    className="typing-container"
+                    label="SAT Date"
+                    type="date"
+                  />
+                </Grid>
+                <h3 style={{ color: '#2656A5' }}>Financial Aid:</h3>
+                <Grid>
+                  <FormControl variant="standard" sx={{ m: 1, minWidth: 200 }}>
+                    <InputLabel id="demo-simple-select-standard-label">
+                      Completed Finacial Aid Process
+                    </InputLabel>
+                    <Select
+                      labelId="demo-simple-select-standard-label"
+                      id="demo-simple-select-standard"
+                      label="Age"
+                      defaultValue={10}
+                    >
+                      <MenuItem value=""></MenuItem>
+                      <MenuItem value={10}>Yes</MenuItem>
+                      <MenuItem value={20}>No</MenuItem>
+                    </Select>
+                  </FormControl>
+                </Grid>
+                <Grid>
+                  <FormControl variant="standard" sx={{ m: 1, minWidth: 200 }}>
+                    <InputLabel id="demo-simple-select-standard-label">
+                      Need Help With FAFSA
+                    </InputLabel>
+                    <Select
+                      labelId="demo-simple-select-standard-label"
+                      id="demo-simple-select-standard"
+                      label="Age"
+                      defaultValue={20}
+                    >
+                      <MenuItem value=""></MenuItem>
+                      <MenuItem value={10}>Yes</MenuItem>
+                      <MenuItem value={20}>No</MenuItem>
+                    </Select>
+                  </FormControl>
+                </Grid>
+                <Grid marginBottom={2}>
+                  <FormControl variant="standard" sx={{ m: 1, minWidth: 200 }}>
+                    <InputLabel id="demo-simple-select-standard-label">
+                      Support Needed
+                    </InputLabel>
+                    <Select
+                      labelId="demo-simple-select-standard-label"
+                      id="demo-simple-select-standard"
+                      label="Age"
+                      defaultValue={10}
+                    >
+                      <MenuItem value=""></MenuItem>
+                      <MenuItem value={10}>Yes</MenuItem>
+                      <MenuItem value={20}>No</MenuItem>
+                    </Select>
+                  </FormControl>
+                </Grid>
               </Grid>
             </Grid>
           </Box>
@@ -369,32 +521,170 @@ function SignUpEdit(props) {
               <Grid item xs={12} style={{ height: '64vh' }}>
                 <GridText>
                   <h3 style={{ color: '#2656A5' }}>Parent Information</h3>
-                  <h5>First Name: Bob</h5>
-                  <h5>Last Name: Doe</h5>
-                  <h5>Address: 12345 Demo Street</h5>
-                  <h5>Parent Apartment Number: 42</h5>
-                  <h5>City: Milwaukee</h5>
-                  <h5>State: Wisconsin</h5>
-                  <h5>Zip Code: 50021</h5>
-                  <h5>Home Phone: 421-422-4123</h5>
-                  <h5>Cell Phone: 234-567-8901</h5>
-                  <h5>Parent Email: doeB@gmail.com</h5>
+                  <Grid marginBottom={2}>
+                    <TextField
+                      size="small"
+                      className="typing-container"
+                      defaultValue="Bob Joe"
+                      label="Name"
+                    />
+                  </Grid>
+
+                  <Grid marginBottom={2}>
+                    <TextField
+                      size="small"
+                      className="typing-container"
+                      defaultValue="12345 demo street"
+                      label="Adress"
+                    />
+                  </Grid>
+                  <Grid marginBottom={2}>
+                    <TextField
+                      size="small"
+                      className="typing-container"
+                      defaultValue="40"
+                      label="Apt. #"
+                    />
+                  </Grid>
+                  <Grid marginBottom={2}>
+                    <TextField
+                      size="small"
+                      className="typing-container"
+                      defaultValue="Milwaukee"
+                      label="City"
+                    />
+                  </Grid>
+                  <Grid marginBottom={2}>
+                    <TextField
+                      size="small"
+                      className="typing-container"
+                      defaultValue="WI"
+                      label="State"
+                    />
+                  </Grid>
+                  <Grid marginBottom={2}>
+                    <TextField
+                      size="small"
+                      className="typing-container"
+                      defaultValue="50021"
+                      label="Zip Code"
+                    />
+                  </Grid>
+                  <Grid marginBottom={2}>
+                    <TextField
+                      size="small"
+                      className="typing-container"
+                      defaultValue="312-315-5646"
+                      label="Home Phone"
+                    />
+                  </Grid>
+                  <Grid marginBottom={2}>
+                    <TextField
+                      size="small"
+                      className="typing-container"
+                      defaultValue="314-654-7821"
+                      label="Cell Phone"
+                    />
+                  </Grid>
+                  <Grid marginBottom={2}>
+                    <TextField
+                      size="small"
+                      className="typing-container"
+                      defaultValue="mdijd@gmail.com"
+                      label="Email"
+                    />
+                  </Grid>
                   <h3 style={{ color: '#2656A5' }}>Guidance Couselor Info:</h3>
-                  <h5>I know my guidance counselor: Yes</h5>
+                  <FormControl variant="standard" sx={{ m: 1, minWidth: 200 }}>
+                    <InputLabel id="demo-simple-select-standard-label">
+                      Knows Guidance Cousoler{' '}
+                    </InputLabel>
+                    <Select
+                      labelId="demo-simple-select-standard-label"
+                      id="demo-simple-select-standard"
+                      label="Age"
+                      defaultValue={10}
+                    >
+                      <MenuItem value=""></MenuItem>
+                      <MenuItem value={10}>Yes</MenuItem>
+                      <MenuItem value={20}>No</MenuItem>
+                    </Select>
+                  </FormControl>
                   <h3 style={{ color: '#2656A5' }}>Admin Details:</h3>
-                  <h5>Activity Status: Active</h5>
-                  <h5>Acceptance Status: Accepted</h5>
+                  <Grid>
+                  <FormControl variant="standard" sx={{ m: 1, minWidth: 200 }}>
+                    <InputLabel id="demo-simple-select-standard-label">
+                      Activity Status{' '}
+                    </InputLabel>
+                    <Select
+                      labelId="demo-simple-select-standard-label"
+                      id="demo-simple-select-standard"
+                      label="Age"
+                      defaultValue={10}
+                    >
+                      <MenuItem value=""></MenuItem>
+                      <MenuItem value={10}>Active</MenuItem>
+                      <MenuItem value={20}>Inactive</MenuItem>
+                    </Select>
+                  </FormControl>
+                  </Grid>
+                  <FormControl variant="standard" sx={{ m: 1, minWidth: 200 }}>
+                    <InputLabel id="demo-simple-select-standard-label">
+                      Acceptance Status{' '}
+                    </InputLabel>
+                    <Select
+                      labelId="demo-simple-select-standard-label"
+                      id="demo-simple-select-standard"
+                      label="Age"
+                      defaultValue={10}
+                    >
+                      <MenuItem value=""></MenuItem>
+                      <MenuItem value={10}>Accepted</MenuItem>
+                      <MenuItem value={20}>Not Accepted</MenuItem>
+                    </Select>
+                  </FormControl>
                   <h3 style={{ color: '#2656A5' }}>Signatures</h3>
-                  <h5>Student Signature: Aadi Tiwari</h5>
-                  <h5>Date Signed: 2/1/22</h5>
-                  <h5>Parent Signature: Bob Doe</h5>
-                  <h5>Date Signed: 2/1/22</h5>
+                  <Grid marginBottom={2}>
+                    <TextField
+                      size="small"
+                      className="typing-container"
+                      defaultValue="Aadi Tiwair"
+                      label="Student Signiture"
+                    />
+                  </Grid>
+                  <Grid marginBottom={2}>
+                    <TextField
+                      size="small"
+                      className="typing-container"
+                     
+                      label="Date Signed"
+                      type='date'
+                    />
+                  </Grid>
+                 
+                  <Grid marginBottom={2}>
+                    <TextField
+                      size="small"
+                      className="typing-container"
+                      defaultValue="John Tiwair"
+                      label="Parent Signiture"
+                    />
+                  </Grid>
+                  <Grid marginBottom={2}>
+                    <TextField
+                      size="small"
+                      className="typing-container"
+                     
+                      label="Date Signed"
+                      type='date'
+                    />
+                  </Grid>
                 </GridText>
               </Grid>
             </Grid>
           </Box>
         </TabPanel>
-      </Box>{' '}
+      </Box>
       <Button variant="contained" onClick={onSaveClick}>
         Save
       </Button>
