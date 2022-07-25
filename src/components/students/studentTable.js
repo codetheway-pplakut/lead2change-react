17469938257389e75b71954d4c76204154812478
Holly R <hraetz14@gmail.com>
2022-07-25 10:41:09 -0500

    Table fixes 1.2.3

--- src/components/students/studentTable.js
+++ src/components/students/studentTable.js
@@ -1,5 +1,9 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
+
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -7,6 +11,7 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import TableSortLabel from '@mui/material/TableSortLabel';
+import Typography from '@mui/material/Typography';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 import { TextField } from '@mui/material';
@@ -15,6 +20,9 @@ import SearchIcon from '@mui/icons-material/Search';
 import Tab from '@mui/material/Tab';
 import Tabs from '@mui/material/Tabs';
 
+import ProgressIndicatorOverlay from '../progress-indicator-overlay/progress-indicator-overlay';
+
+import CoachAssignModal from './student-coach-assign-modal';
 import StudentRegistryModal from './studentRegistryModal';
 import StudentModal from './studentModal';
 import {
@@ -24,6 +32,72 @@ import {
   updateStudent,
 } from '../../services/students/students';
 import getCoachById from '../../services/coaches/coaches';
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 0 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+const refreshPage = async () => {
+  window.location.reload(true);
+};
+
+const deactivateHandler = async (studentId) => {
+  const updatedStudent = await getStudentById(studentId);
+  updatedStudent.state = 'inactive';
+  await updateStudent(updatedStudent);
+  refreshPage();
+};
+
+const activateHandler = async (studentId) => {
+  const updatedStudent = await getStudentById(studentId);
+  updatedStudent.state = 'active';
+  await updateStudent(updatedStudent);
+  refreshPage();
+};
+
+const declineHandler = async (studentId) => {
+  const updatedStudent = await getStudentById(studentId);
+  updatedStudent.state = 'rejected';
+  await updateStudent(updatedStudent);
+  refreshPage();
+};
+
+const reassignCoachHandler = async (studentId, coachId) => {
+  const updatedStudent = await getStudentById(studentId);
+  updatedStudent.coachId = coachId;
+  await updateStudent(updatedStudent);
+  refreshPage();
+};
+
+TabPanel.propTypes = {
+  // eslint-disable-next-line react/require-default-props
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `full-width-tab-${index}`,
+    'aria-controls': `full-width-tabpanel-${index}`,
+  };
+}
 
 function descendingComparator(a, b, orderBy) {
   if (b[orderBy] < a[orderBy]) {
@@ -42,10 +116,27 @@ function getComparator(order, orderBy) {
 }
 
 export default function StudentTable() {
+  const [students, setStudents] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [search, setSearch] = useState('');
+
+  const refreshStudents = async () => {
+    setIsLoading(true);
+    const response = await getStudents();
+
+    setIsLoading(false);
+    setStudents(response);
+  };
+  useEffect(() => {
+    refreshStudents();
+  }, []);
+
+  const [tabValue, setTabValue] = useState(0);
+  const handleTabChange = (event, newValue) => {
+    setTabValue(newValue);
+  };
   const [order, setOrder] = useState('asc');
   const [orderBy, setOrderBy] = useState('name');
-  const [search, setSearch] = useState('');
-  const [students, setStudents] = useState([]);
   const [value, setValue] = useState(0);
 
   const handleRequestSort = (event, property) => {
@@ -54,47 +145,52 @@ export default function StudentTable() {
     setOrderBy(property);
   };
 
-  const handleTabChange = () => {
-
-  };
-
   return (
-    <Paper sx={{ width: '100%' }}>
-      <Grid container spacing={1} alignItems="center">
-        <Grid item xs={8}>
-          <Tabs value={value} onChange={handleTabChange}>
-            <Tab label="Active" />
-            <Tab label="Inactive" />
-            <Tab label="Applied" />
+    <Box sx={{ width: '100%', height: '60%' }}>
+      <ProgressIndicatorOverlay active={isLoading} />
+      <Box sx={{ bgcolor: 'background.paper', width: '25%' }}>
+        <AppBar position="static">
+          <Tabs
+            value={tabValue}
+            onChange={handleTabChange}
+            textColor="inherit"
+            variant="fullWidth"
+            TabIndicatorProps={{
+              style: {
+                backgroundColor: '#FFFFFF',
+                height: '3px',
+              },
+            }}
+          >
+            <Tab label="Active" {...a11yProps(0)} />
+            <Tab label="Inactive" {...a11yProps(1)} />
+            <Tab label="Applicants" {...a11yProps(2)} />
           </Tabs>
-        </Grid>
-        <Grid item xs={2}>
-          <TextField
-            value={search}
-            placeholder="Search..."
-            variant="outlined"
-            size="small"
-            margin="normal"
-            align="right"
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position="start">
-                  <SearchIcon />
-                </InputAdornment>
-              ),
-            }}
-            onChange={(e) => {
-              setSearch(e.target.value);
-            }}
-          />
-        </Grid>
-        <Grid item xs={2} align="right" padding={2}>
-          <StudentRegistryModal />
-        </Grid>
-      </Grid>
-      <TableContainer>
-        <Table sx={{ minWidth: 750 }}>
-          {/* <EnhancedTableHead
+        </AppBar>
+      </Box>
+      <TextField
+        value={search}
+        placeholder="Search..."
+        variant="outlined"
+        size="small"
+        margin="normal"
+        align="right"
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <SearchIcon />
+            </InputAdornment>
+          ),
+        }}
+        onChange={(e) => {
+          setSearch(e.target.value);
+        }}
+      />
+      <StudentRegistryModal />
+      <TabPanel value={tabValue} index={0}>
+        <TableContainer component={Paper} sx={{ height: '69vh' }}>
+          <Table sx={{ minWidth: 10 }} stickyHeader>
+            {/* <EnhancedTableHead
             order={order}
             orderBy={orderBy}
             onRequestSort={handleRequestSort}
@@ -148,8 +244,9 @@ export default function StudentTable() {
               })}
           </TableBody>
             */}
-        </Table>
-      </TableContainer>
-    </Paper>
+          </Table>
+        </TableContainer>
+      </TabPanel>
+    </Box>
   );
 }
