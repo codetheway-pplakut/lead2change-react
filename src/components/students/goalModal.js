738e3ec9d55acb2b62d721ce510b3e7aabd8c001
Noah Ren <nren2025@gmail.com>
2022-07-26 12:21:19 -0500

    changed styling to vh

--- src/components/students/goalModal.js
+++ src/components/students/goalModal.js
@@ -21,11 +21,11 @@ export default function GoalRegistryModal(props) {
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
-    width: 600,
+    width: '60vh',
     bgcolor: 'background.paper',
     border: '',
-    boxShadow: 24,
-    p: 4,
+    boxShadow: '12vh',
+    p: '2vh',
   };
 
   const { addFunction } = props;
