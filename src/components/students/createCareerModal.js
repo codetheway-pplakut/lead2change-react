4514f21ecae4bd647f886881404d08113741f765
Aaditya Tiwari (BCHS) <24aaditf@gmail.com>
2022-07-27 10:37:19 -0500

    Career Modal

--- src/components/students/createCareerModal.js
+++ src/components/students/createCareerModal.js
@@ -1,14 +1,27 @@
 import React, { useState } from 'react';
-import Typography from '@mui/material/Typography';
-import Modal from '@mui/material/Modal';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import CloseIcon from '@mui/icons-material/Close';
 import Grid from '@mui/material/Grid';
-import TextField from '@mui/material/TextField';
 import IconButton from '@mui/material/IconButton';
-import CloseOutlinedIcon from '@mui/icons-material/CloseOutlined';
+import Modal from '@mui/material/Modal';
+import Stack from '@mui/material/Stack';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
 import PropTypes from 'prop-types';
-import ColorButton from '../coaches/Shared/ColoredButton';
 
 export default function CreateCareerModal(props) {
+  const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: '60vh',
+    bgcolor: 'background.paper',
+    border: '',
+    boxShadow: '12vh',
+    p: '2vh',
+  };
   const { addFunction } = props;
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
@@ -31,117 +44,92 @@ export default function CreateCareerModal(props) {
 
   return (
     <div>
-      <ColorButton variant="contained" onClick={handleOpen}>
-        + New Career
-      </ColorButton>
+      <Button onClick={handleOpen}>Create a New Career</Button>
       <Modal
         open={open}
         onClose={handleClose}
         aria-labelledby="modal-modal-title"
       >
-        <Grid container variant="large">
-          <Grid container>
-            <Grid item sx={{ bgcolor: '#004cbb', color: 'white' }} xs={12}>
-              <Grid container alignItems="center" sx={{ margin: 1 }}>
-                <Grid item xs={2} />
-                <Grid item xs={8}>
+        <Modal open={open} onClose={handleClose}>
+          <Box sx={style}>
+            <Box>
+              <Grid container>
+                <Grid item xs={11}>
                   <Typography variant="h5" component="h2" align="center">
                     New Career
                   </Typography>
                 </Grid>
-                <Grid item xs={1} />
-                <Grid item>
-                  <IconButton
-                    align="right"
-                    size="medium"
-                    onClick={handleClose}
-                    sx={{ color: 'white' }}
-                  >
-                    <CloseOutlinedIcon />
+                <Grid item xs={1}>
+                  <IconButton color="primary" onClick={handleClose}>
+                    <CloseIcon />
                   </IconButton>
                 </Grid>
               </Grid>
-            </Grid>
-          </Grid>
-          <Grid container>
-            <Grid item xs={12}>
-              <Grid container spacing={1} sx={{ p: 2 }} justifyContent="center">
-                <Grid item xs={12}>
-                  <TextField
-                    value={collegeBound}
-                    fullWidth
-                    label="Are you College Bound?"
-                    variant="outlined"
-                    size="small"
-                    onChange={(e) => {
-                      setCollegeBound(e.target.value);
-                    }}
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField
-                    value={careerCluster}
-                    fullWidth
-                    label="Enter your Career Cluster"
-                    variant="outlined"
-                    size="small"
-                    onChange={(e) => {
-                      setCareerCluster(e.target.value);
-                    }}
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField
-                    value={specificCareer}
-                    fullWidth
-                    label="What is your specific career of choice?"
-                    variant="outlined"
-                    size="small"
-                    onChange={(e) => {
-                      setSpecificCareer(e.target.value);
-                    }}
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField
-                    value={technicalCollegeBound}
-                    fullWidth
-                    label="Are you Technical College Bound?"
-                    variant="outlined"
-                    size="small"
-                    onChange={(e) => {
-                      setTechnicalCollegeBound(e.target.value);
-                    }}
-                  />
-                </Grid>
-                <Grid item xs={2}>
-                  <ColorButton
-                    variant="contained"
-                    fullWidth
-                    onClick={Create}
-                    value={
-                      (collegeBound,
-                      careerCluster,
-                      specificCareer,
-                      technicalCollegeBound)
-                    }
-                  >
-                    Create
-                  </ColorButton>
-                </Grid>
-                <Grid item xs={2}>
-                  <ColorButton
-                    variant="contained"
-                    fullWidth
-                    onClick={handleClose}
-                  >
-                    Cancel
-                  </ColorButton>
-                </Grid>
-              </Grid>
-            </Grid>
-          </Grid>
-        </Grid>
+            </Box>
+            <Box>
+              <Stack direction="row" spacing={2} sx={{ my: 2 }}>
+                <TextField
+                  value={collegeBound}
+                  fullWidth
+                  label="Are you College Bound?"
+                  variant="outlined"
+                  size="small"
+                  onChange={(e) => {
+                    setCollegeBound(e.target.value);
+                  }}
+                />
+                <TextField
+                  value={careerCluster}
+                  fullWidth
+                  label="Enter your Career Cluster"
+                  variant="outlined"
+                  size="small"
+                  onChange={(e) => {
+                    setCareerCluster(e.target.value);
+                  }}
+                />
+              </Stack>
+            </Box>
+            <Box>
+              <Stack direction="column" spacing={2} sx={{ my: 2 }}>
+                <TextField
+                  value={specificCareer}
+                  fullWidth
+                  label="What is your specific career of choice?"
+                  variant="outlined"
+                  size="small"
+                  onChange={(e) => {
+                    setSpecificCareer(e.target.value);
+                  }}
+                />
+                <TextField
+                  value={technicalCollegeBound}
+                  fullWidth
+                  label="Are you Technical College Bound?"
+                  variant="outlined"
+                  size="small"
+                  onChange={(e) => {
+                    setTechnicalCollegeBound(e.target.value);
+                  }}
+                />
+              </Stack>
+            </Box>
+            <Button
+              variant="contained"
+              fullWidth
+              onClick={Create}
+              type="submit"
+              value={
+                (collegeBound,
+                careerCluster,
+                specificCareer,
+                technicalCollegeBound)
+              }
+            >
+              Create Career
+            </Button>
+          </Box>
+        </Modal>
       </Modal>
     </div>
   );
