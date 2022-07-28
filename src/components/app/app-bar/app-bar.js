cd15e1b6de81eb100196ebcf06bf77eed9256cf5
Aniket Singh <asingh11771@gmail.com>
2022-07-28 10:35:24 -0500

    Nav Bar Fix

--- src/components/app/app-bar/app-bar.js
+++ src/components/app/app-bar/app-bar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useNavigate } from 'react-router';
+import { useNavigate, useLocation } from 'react-router-dom';
 import Menu from '@mui/icons-material/Menu';
 import MaterialAppBar from '@mui/material/AppBar';
 import IconButton from '@mui/material/IconButton';
@@ -11,6 +11,7 @@ import PropTypes from 'prop-types';
 import ROUTES from '../../../constants/routes';
 
 export default function AppBar(props) {
+  const location = useLocation();
   const navigate = useNavigate();
   const { onMenuClick, title } = props;
 
@@ -31,10 +32,10 @@ export default function AppBar(props) {
   };
   const onLogoutClick = () => {
     navigate(ROUTES.LOGIN);
-    localStorage.removeItem('token'); // I don't know backend
+    // localStorage.removeItem('token');
   };
 
-  return (
+  return location.pathname === '/' ? (
     <MaterialAppBar>
       <Toolbar>
         <IconButton
@@ -42,61 +43,128 @@ export default function AppBar(props) {
           edge="start"
           onClick={onMenuClick}
           size="large"
-          sx={{ mr: 1 }}
         >
           <Menu />
         </IconButton>
-        <Button variant="text" onClick={onHomeClick} justify="center">
-          {title && (
-            <Typography variant="h6" color="white">
-              {title}
-            </Typography>
-          )}
-        </Button>
-        {/* {title && <Typography variant="h6">{title}</Typography>} */}
-        <Grid item xl={8} lg={8} md={8} sm={8} xs={8}>
-          <Grid container spacing={2} alignItems="center" justify="center">
-            <Grid item xl={2} sx={{ ml: 2 }}>
-              <Button variant="text" onClick={onStudentClick} justify="center">
-                <Typography color="white" variant="body2">
-                  Students
+        <Grid
+          container
+          spacing={2}
+          alignItems="center"
+          justifyContent="flex-start"
+        >
+          <Grid item xs="auto">
+            <Button variant="text">
+              {title && (
+                <Typography variant="h6" color="white">
+                  {title}
                 </Typography>
-              </Button>
-            </Grid>
-            <Grid item xl={2}>
-              <Button variant="text" onClick={onCoachesClick} justify="center">
-                <Typography color="white" variant="body2">
-                  Coaches
-                </Typography>
-              </Button>
-            </Grid>
-            <Grid item xl={2}>
-              <Button
-                variant="text"
-                onClick={onInterviewsClick}
-                justify="center"
-              >
-                <Typography color="white" variant="body2">
-                  Interviews
-                </Typography>
-              </Button>
-            </Grid>
-            <Grid item xl={2}>
-              <Button variant="text" onClick={onSignUpClick} justify="center">
-                <Typography color="white" variant="body2">
-                  Sign Up
-                </Typography>
-              </Button>
-            </Grid>
-            <Grid item xl={2} sx={{ ml: 52 }}>
-              <Button variant="text" onClick={onLogoutClick} justify="center">
-                <Typography color="white" variant="body2">
-                  <span>LOG OUT: ***</span>
-                </Typography>
-              </Button>
-            </Grid>
+              )}
+            </Button>
+          </Grid>
+          <Grid item>
+            <Button variant="text">
+              <Typography color="white" variant="body2">
+                Students
+              </Typography>
+            </Button>
+          </Grid>
+          <Grid item>
+            <Button variant="text">
+              <Typography color="white" variant="body2">
+                Coaches
+              </Typography>
+            </Button>
+          </Grid>
+          <Grid item>
+            <Button variant="text">
+              <Typography color="white" variant="body2">
+                Interviews
+              </Typography>
+            </Button>
+          </Grid>
+          <Grid item>
+            <Button variant="text">
+              <Typography color="white" variant="body2">
+                Sign Up
+              </Typography>
+            </Button>
           </Grid>
         </Grid>
+        {/* <Grid container alignItems="center" justifyContent="flex-end">
+          <Grid xs="auto">
+            <Button variant="text">
+              <Typography color="white" variant="body2">
+                <span>LOG OUT: ***</span>
+              </Typography>
+            </Button>
+          </Grid>
+        </Grid> */}
+      </Toolbar>
+    </MaterialAppBar>
+  ) : (
+    <MaterialAppBar>
+      <Toolbar>
+        <IconButton
+          color="inherit"
+          edge="start"
+          onClick={onMenuClick}
+          size="large"
+        >
+          <Menu />
+        </IconButton>
+        <Grid
+          container
+          spacing={2}
+          alignItems="center"
+          justifyContent="flex-start"
+        >
+          <Grid item xs="auto">
+            <Button variant="text" onClick={onHomeClick}>
+              {title && (
+                <Typography variant="h6" color="white">
+                  {title}
+                </Typography>
+              )}
+            </Button>
+          </Grid>
+          <Grid item>
+            <Button variant="text" onClick={onStudentClick}>
+              <Typography color="white" variant="body2">
+                Students
+              </Typography>
+            </Button>
+          </Grid>
+          <Grid item>
+            <Button variant="text" onClick={onCoachesClick}>
+              <Typography color="white" variant="body2">
+                Coaches
+              </Typography>
+            </Button>
+          </Grid>
+          <Grid item>
+            <Button variant="text" onClick={onInterviewsClick}>
+              <Typography color="white" variant="body2">
+                Interviews
+              </Typography>
+            </Button>
+          </Grid>
+          <Grid item>
+            <Button variant="text" onClick={onSignUpClick}>
+              <Typography color="white" variant="body2">
+                Sign Up
+              </Typography>
+            </Button>
+          </Grid>
+        </Grid>
+        {/* <Grid container alignItems="center" justifyContent="flex-end">
+          <Grid xs="auto">
+            <Button variant="text" onClick={onLogoutClick}>
+              <Typography color="white" variant="body2">
+                <span>LOG OUT: ***</span>
+              </Typography>
+            </Button>
+          </Grid>
+        </Grid> */}
       </Toolbar>
     </MaterialAppBar>
   );
