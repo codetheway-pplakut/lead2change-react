99bb0d68b969e4321622827112582f6e6b8c44f4
Aniruddh <aniruddhmayya@gmail.com>
2022-08-01 14:55:43 -0500

    ErrorPage Progress

---
+++ src/components/login/ErrorPage.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+
+function ErrorPage(props) {
+  return (
+    <Container maxWidth="sm">
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <Typography
+          align="center"
+          color="text.primary"
+          component="h1"
+          gutterBottom
+          variant="h2"
+          sx={{ mt: 5 }}
+        >
+          Error 404
+        </Typography>
+      </Box>
+    </Container>
+  );
+}
+
+export default ErrorPage;
