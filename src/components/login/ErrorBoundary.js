9077fb932f3335973dedd98e4b29effff7f91edd
Jihong <jihongbae2nd@gmail.com>
2022-08-02 12:08:58 -0500

    error boundaries update 1.0.2

--- src/components/login/ErrorBoundary.js
+++ src/components/login/ErrorBoundary.js
@@ -1,25 +1,38 @@
-// import { Component } from 'react';
-// import ErrorPage from './ErrorPage';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import ErrorPage from './ErrorPage';
 
-// class ErrorBoundary extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = { hasError: false };
-//   }
+class ErrorBoundary extends Component {
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
 
-//   static getDerivedStateFromError(error) {
-//     return { hasError: true };
-//   }
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
 
-//   componentDidCatch(error, errorInfo) {
-//     console.log(error, errorInfo);
-//   }
+  componentDidCatch(error, errorInfo) {
+    console.log(error, errorInfo);
+  }
 
-//   render() {
-//     if (this.state.hasError) {
-//       return <ErrorPage />;
-//     }
+  render() {
+    const { children } = this.props;
+    const { hasError } = this.state;
 
-//     return this.props.children;
-//   }
-// }
+    if (hasError) return <ErrorPage />;
+
+    return children;
+  }
+}
+
+ErrorBoundary.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.node,
+    PropTypes.arrayOf(PropTypes.node),
+  ]),
+};
+
+ErrorBoundary.defaultProps = {
+  children: null,
+};
