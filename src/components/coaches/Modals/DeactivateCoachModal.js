63c07612f854c1c102cd9525039a2b1a87f31ecb
Abby Winn <abbywinn14@gmail.com>
2022-08-04 01:03:49 -0500

    Deactivate/Reactivate/Delete modals standardized (except students)

--- src/components/coaches/Modals/DeactivateCoachModal.js
+++ src/components/coaches/Modals/DeactivateCoachModal.js
@@ -3,8 +3,11 @@ import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
 import Grid from '@mui/material/Grid';
 import IconButton from '@mui/material/IconButton';
-import CloseOutlinedIcon from '@mui/icons-material/CloseOutlined';
+import Box from '@mui/material/Box';
+import CloseIcon from '@mui/icons-material/Close';
 import PropTypes from 'prop-types';
+import Button from '@mui/material/Button';
+import { createTheme } from '@mui/material/styles';
 import ColorButton from '../Shared/ColoredButton';
 
 export default function InactivationModal(props) {
@@ -16,56 +19,108 @@ export default function InactivationModal(props) {
     handleClose();
     unassignFunction(coach);
   };
+
+  const closeIconColor = {
+    color: 'white',
+  };
+  const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    width: 430,
+    transform: 'translate(-50%, -50%)',
+    bgcolor: 'background.paper',
+    borderRadius: '10px',
+    boxShadow: 24,
+  };
+
+  const buttonTheme = createTheme({
+    palette: {
+      delete: {
+        main: '#004cbb',
+        contrastText: '#fff',
+      },
+      cancel: {
+        main: '#004cbb',
+        contrastText: '#fff',
+      },
+    },
+  });
+
   return (
     <div>
       <ColorButton variant="contained" onClick={handleOpen}>
         Deactivate
       </ColorButton>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-      >
-        <Grid container variant="small">
+      <Modal open={open} onClose={handleClose}>
+        <Box sx={style}>
           <Grid container>
-            <Grid item sx={{ bgcolor: '#2656A5', color: 'white' }} xs={12}>
+            <Grid
+              item
+              sx={{
+                bgcolor: '#004cbb',
+                color: 'white',
+                borderTopLeftRadius: 10,
+                borderTopRightRadius: 10,
+              }}
+              xs={12}
+            >
               <Grid container alignItems="center" sx={{ margin: 1 }}>
                 <Grid item xs={2} />
                 <Grid item xs={8}>
-                  <Typography variant="h5" component="h2" align="center">
+                  <Typography
+                    variant="h5"
+                    component="h2"
+                    align="center"
+                    padding="10px"
+                  >
                     Deactivate Coach
                   </Typography>
-                </Grid>
-                <Grid item>
                   <IconButton
-                    align="right"
-                    size="medium"
                     onClick={handleClose}
-                    sx={{ color: 'white' }}
+                    sx={{ position: 'absolute', right: 8, top: 8 }}
                   >
-                    <CloseOutlinedIcon />
+                    <CloseIcon fontSize="large" sx={closeIconColor} />
                   </IconButton>
                 </Grid>
+                <Grid item xs={1} />
               </Grid>
             </Grid>
           </Grid>
           <Grid container spacing={1} sx={{ p: 2 }} justifyContent="center">
-            <Grid item xs={12} align="center">
-              Are you sure you want to deactivate {coach.coachFirstName}{' '}
-              {coach.coachLastName}?
+            <Box padding="30px">
+              <Grid item xs={12} align="center">
+                Are you sure you want to deactivate {coach.coachFirstName}{' '}
+                {coach.coachLastName}?
+              </Grid>
+            </Box>
+            <Grid item xs={4}>
+              <Button
+                theme={buttonTheme}
+                color="delete"
+                variant="contained"
+                onClick={Inactivate}
+                style={{ minWidth: '120px' }}
+              >
+                <Typography paddingTop="5px" paddingBottom="5px">
+                  Deactivate
+                </Typography>
+              </Button>
             </Grid>
-            <Grid item xs={6}>
-              <ColorButton variant="contained" fullWidth onClick={Inactivate}>
-                Deactivate
-              </ColorButton>
-            </Grid>
-            <Grid item xs={6}>
-              <ColorButton variant="contained" fullWidth onClick={handleClose}>
-                Cancel
-              </ColorButton>
+            <Grid xs={1} />
+            <Grid item xs={4}>
+              <Button
+                theme={buttonTheme}
+                color="cancel"
+                variant="contained"
+                onClick={handleClose}
+                style={{ minWidth: '120px' }}
+              >
+                <Typography padding="5px">Cancel</Typography>
+              </Button>
             </Grid>
           </Grid>
-        </Grid>
+        </Box>
       </Modal>
     </div>
   );
